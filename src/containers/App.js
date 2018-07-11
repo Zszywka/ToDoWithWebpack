90298058e409578746696e53e1119fd4d9e419df
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-07-11 11:50:46 +0200

    Init project

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+// wszytskie pobrane pakiety tak sie pobiera do tego pliku
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+
+class App extends React.Component {
+  constructor(props){
+      super(props);
+      //utrzymuje stan aplikacji
+      this.state = {
+          data: []
+      };
+  }
+  // dodawanie nowych elementow do kolekcji
+  addTodo(val){
+      const todo = {
+          text: val,
+          // bibloteka uuid do tworzenia unikatowtych wartosci
+          id: uuid.v4(),
+      };
+      //ustawienie stanu aplikacji
+      //const aby zmienna nie byla nigdy nadpisywana
+      //data zwraca(jak push) [tablice elementami kolekcji + na koncu(todo) nowy element kolekcji]
+      //i nie zmienia stanu (bo push zmienia stan)
+      //dostajemy nowa tablice z zawartoscia starej i nowym elem na koncu
+      const data = [...this.state.data, todo];
+      //this.setState({data: data}); to samo
+      this.setState({data});
+  }
+  //usuwanie elementow
+  removeTodo(id) {
+    //dzieki niej tworzyny nowa tablice z odfiltrowanymi pewnymi elementami
+    //?? skad bierze todo? z powyzszej metody?jak to dokladnie dziala???
+    //funkcja bierze cala tablice elementow filtruje(sprawdza czy id aktualnie
+    // sprawdzanego elementu(id) jest = id usuwanego elementu(todo.id))
+  const remainder = this.state.data.filter(todo => todo.id !== id);
+  this.setState({data: remainder});
+  }
+
+  render() {
+    return (
+      <div className={style.TodoApp}>
+          Tutaj pojawią się komponenty naszej aplikacji.
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+
+module.exports = {
+  //zaczynamy kompilacje od:
+    entry: './src/index.js',
+    output: {
+      // sciezka kompilacji
+        path: path.resolve(__dirname, 'build'),
+        // plik wyjsciowy
+        filename: 'app.bundle.js'
+    }
+};
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.bundle.js'
+    },
+    // testuje czy modul ma rozszezenie .js , a gdy ma, transforujemy go przy uzyciu babel-loader
+    module: {
+      // tablica loaderow
+      rules: [
+        {
+          // warunek, który musi zostać spełniony w nazwie importowanego modułu
+          // aby mógł zostać przetransformowany danym loaderem
+          test: /\.js$/, //o co chodzi z rozszerzeniem tego $????
+          // mozna wpisac tu include lub exclude aby wlaczyc dany modu lub wylaczyc z loadera
+          //z jakis loaderow ma skorzystac webpack gdy spelniony zostanie warunek wyzej
+          exclude: '/node_modules',
+          loader: "babel-loader"
+          // mozna wpisac rules:[tablica loaderow gdy chcemy skorzystac z wiecej niz jednego]
+          // to samo co zapisane w .babelrc
+          // options: {
+          //   presets: ['env', 'react', 'uuid']
+          // }
+        },
+        { //wszystko z rozszezeniem .css
+          test: /\.css$/,
+          use: [ //to lista loaderow przez jakie musi przejsc plik zeby stac sie modulem(sciezka zdrowia)
+            {loader: 'style-loader'},
+            //modules:true, zmienia zasieg na lokalny(dziala tylko w modulegdzie zostal zaiportowany)
+            // gdzie indziej mozna uzyc klasy o tej samej nazwie do innych styli
+            {loader: 'css-loader', options: {modules: true}}
+          ]
+        }
+      ]
+    }
+};
