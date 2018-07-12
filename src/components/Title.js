5a97d912375dc28bdd4eeb6db3bfe3cbcfc37888
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-07-12 18:05:13 +0200

    Fix the bugs

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,8 +7,6 @@ class Title extends React.Component {
 
   render() {
     return (
-      // <h1>{props.title}</h1>
-      // <h3>Number of task: {props.number}</h3>  ??kiedy props a kiedy this.props
       <div>
         <h1>ToDo</h1>
         <h3>Number of task: {this.props.data.length}</h3>
--- src/containers/App.js
+++ src/containers/App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
   render() {
     return (
       <div className={style.TodoApp}>
-        // <Title title="ToDo" number={this.state.data.length}></Title>
+        
         <Title data={this.state.data}/>
       </div>
     );
